a9b8d29ecc8b86b3d839e5a2b7a38017b1aec64b
George Tian <me@georgetian.com>
2025-02-16 18:24:35 +1100

    create db

--- models/database.py
+++ models/database.py
@@ -2,8 +2,11 @@ import asyncio
 from dataclasses import asdict
 from contextlib import asynccontextmanager
 
+import asyncpg
 from sqlalchemy import URL, create_engine
 import sqlalchemy
+import sqlalchemy.dialects
+import sqlalchemy.dialects.postgresql
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
@@ -11,7 +14,7 @@ from sqlmodel import SQLModel
 from config import DatabaseConfig
 from models.models import *
 from services.logging import get_logger
-
+from sqlalchemy.exc import ProgrammingError
 
 logger = get_logger(__name__)
 
@@ -26,16 +29,15 @@ class Database:
 
     async def create(self):
         url = self._url._replace(database=None)
-        async with create_async_engine(url).execution_options(isolation_level="AUTOCOMMIT").connect() as conn:
-            try:
-                result = await conn.execute(
-                    sqlalchemy.text(f"SELECT 1 FROM pg_database WHERE datname = '{self._url.database}'")
-                )
-                if result.scalar() is None:
+        # No need to create DB for sqlite
+        if 'sqlite' not in url.drivername:
+            async with create_async_engine(url).execution_options(isolation_level="AUTOCOMMIT").connect() as conn:
+                try:
                     await conn.execute(sqlalchemy.text(f"CREATE DATABASE {self._url.database}"))
                     logger.info(f"Database {self._url.database} created ")
-            except Exception as e:
-                logger.exception(f"Error checking or creating the database: {e}")
+                except Exception as e:
+                    if "already exists" not in str(e):
+                        logger.exception("Error creating database")
         async with self._engine.begin() as conn:
             await conn.run_sync(SQLModel.metadata.create_all)
 
