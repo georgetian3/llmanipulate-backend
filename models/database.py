b22c49f9a9668012ca64a5b440fb6c2b6c6665c4
George Tian <me@georgetian.com>
2025-02-16 18:17:32 +1100

    create db

--- models/database.py
+++ models/database.py
@@ -2,13 +2,18 @@ import asyncio
 from dataclasses import asdict
 from contextlib import asynccontextmanager
 
-from sqlalchemy import URL
+from sqlalchemy import URL, create_engine
+import sqlalchemy
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 
 from config import DatabaseConfig
 from models.models import *
+from services.logging import get_logger
+
+
+logger = get_logger(__name__)
 
 class Database:
 
@@ -20,6 +25,17 @@ class Database:
         )
 
     async def create(self):
+        url = self._url._replace(database=None)
+        async with create_async_engine(url).execution_options(isolation_level="AUTOCOMMIT").connect() as conn:
+            try:
+                result = await conn.execute(
+                    sqlalchemy.text(f"SELECT 1 FROM pg_database WHERE datname = '{self._url.database}'")
+                )
+                if result.scalar() is None:
+                    await conn.execute(sqlalchemy.text(f"CREATE DATABASE {self._url.database}"))
+                    logger.info(f"Database {self._url.database} created ")
+            except Exception as e:
+                logger.exception(f"Error checking or creating the database: {e}")
         async with self._engine.begin() as conn:
             await conn.run_sync(SQLModel.metadata.create_all)
 
